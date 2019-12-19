fcdcbe532992acdbd866320aae800332aa496c77
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-12-19 20:27:28 -0300

    Ajustes Iniciais do Template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,12 @@
-<p>Home Admin</p>
+@extends('adminlte::page')
+
+@section('title', 'CMS :: Painel de Controle ::')
+
+@section('content_header')
+    <h1>Painel de Controle</h1>
+@endsection
+
+@section('content')
+     Conteudo
+@endsection
+
